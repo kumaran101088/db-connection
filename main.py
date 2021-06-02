1680e69704c8c033ca36225768adc667c113aab1
Kumaran <kumaran101088@gmail.com>
2021-06-02 18:00:37 +0530

    adding insert query

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import psycopg2
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 unix_socket = f'/cloudsql/second-flask-run:asia-south1:flask-sql'
 
@@ -20,16 +20,31 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
-@app.route('/data')
+@app.route('/data', methods=['GET', 'POST'])
 def index():
+    if request.method == 'GET':
+        # connection = psycopg2.connect(dbname='checking', user='flaskuser', password='password', host='35.200.145.113')
+        connection = psycopg2.connect(dbname='checking', user='flaskuser', password='password', host='/cloudsql/second-flask-run:asia-south1:flask-sql')
+        cursor = connection.cursor()
+        cursor.execute('SELECT * FROM simple;')
+        data = [row for row in cursor]
+        cursor.close()
+        connection.close()
+        return render_template('index.html', data=data)
+    
+    given_name = request.form.get('name')
     # connection = psycopg2.connect(dbname='checking', user='flaskuser', password='password', host='35.200.145.113')
-    connection = psycopg2.connect(dbname='checking', user='flaskuser', password='password', host=unix_socket)
+    connection = psycopg2.connect(dbname='checking', user='flaskuser', password='password', host='/cloudsql/second-flask-run:asia-south1:flask-sql')
     cursor = connection.cursor()
+    cursor.execute(f"INSERT INTO simple (name) VALUES ('{given_name}')")
+    connection.commit()
     cursor.execute('SELECT * FROM simple;')
     data = [row for row in cursor]
     cursor.close()
     connection.close()
     return render_template('index.html', data=data)
+
+    
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080, host='127.0.0.1')
