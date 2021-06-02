93313617451c07e935edb92b5e24f4dea2326f7d
Kumaran <kumaran101088@gmail.com>
2021-06-02 15:38:50 +0530

    adding instance connection name

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import psycopg2
 from flask import Flask, render_template
 
+unix_socket = f'/cloudsql/{second-flask-run:asia-south1:flask-sql}'
+
 def get_data_from_cloud_sql():
     # connection = psycopg2.connect(dbname='checking', user='flaskuser', password='password', host='35.200.145.113')
-    connection = psycopg2.connect(dbname='checking', user='flaskuser', password='password', host='/cloudsql/flask-sql/.s.PGSQL.5432')
+    # connection = psycopg2.connect(dbname='checking', user='flaskuser', password='password', host='/cloudsql/flask-sql/.s.PGSQL.5432')
+    connection = psycopg2.connect(dbname='checking', user='flaskuser', password='password', host=unix_socket)
     cursor = connection.cursor()
     cursor.execute('SELECT * FROM simple;')
     data = [row for row in cursor]
